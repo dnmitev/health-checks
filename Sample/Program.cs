7f4a00e735c766c12caecf3a041fb6c396dc9018
Dimitar Mitev <dimitar.mitev@experian.com>
2021-08-23 18:21:41 +0300

    wip

---
+++ Sample/Program.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Sample
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                 .ConfigureLogging((ctx, logging) =>
+                 {
+                     var loggingSection = ctx.Configuration.GetSection("Logging");
+
+                     logging.AddConfiguration(loggingSection);
+
+                     logging.ClearProviders();
+                     logging.AddConsole(cfg => cfg.TimestampFormat = "[dd/MM/yyyy HH:mm:ss]");
+                 })
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder
+                        .UseKestrel()
+                        .UseStartup<Startup>();
+                });
+    }
+}
